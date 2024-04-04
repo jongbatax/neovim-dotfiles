4f33f80b3348189ba65ac6ca97f10be20bf3a72a
Herri Siagian <herry.it.2007@gmail.com>
2024-04-04 21:21:28 +0700

    Adding configuration tailwind fold

--- after/plugin/tailwindfold.cfg.lua
+++ after/plugin/tailwindfold.cfg.lua
@@ -1,1 +1,12 @@
-require('tailwind-fold').setup()
+require('tailwind-fold').setup({
+  enabled = true,
+  min_chars = 50,
+  ft = {
+    "html",
+    "blade",
+    "tsx",
+    "php"
+  },
+  conceallevel=2,
+  concealcursor=""
+})
